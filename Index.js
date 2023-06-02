eef31e2960e611b61e02a2120ecd389f71a846aa
Robert <16Robertoguerrero@gmail.com>
2023-06-01 19:36:04 -0500

    terminado about_me e inicio de tech_skills

---
+++ Index.js
@@ -0,0 +1,4 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".header_content");
+    header.classList.toggle("header_content-down", window.scrollY > 600)
+})
