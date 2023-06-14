ee04a4cfdd370c8b40e057feb35c668d5b8f88be
Robert <16Robertoguerrero@gmail.com>
2023-06-14 00:17:13 -0500

    terminado (sin responsive)

--- Index.js
+++ Index.js
@@ -1,27 +1,34 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector(".header_content");
-    header.classList.toggle("header_content-down", window.scrollY > 0)
-}) 
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.slider_section');
+let sliderSectionLast = sliderSection[sliderSection.length -1];
 
-const slider = document.querySelector('.slider_content');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const slideWidth = slider.querySelector('.slider_element').offsetWidth;
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
 
-let currentPosition = 0;
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
-prevButton.addEventListener('click', () => {
-  currentPosition += slideWidth;
-  if (currentPosition > 0) {
-    currentPosition = -(slider.scrollWidth - slider.offsetWidth);
-  }
-  slider.style.transform = `translateX(${currentPosition}px)`;
-});
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll('.slider_section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = 'all 0.5s';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
 
-nextButton.addEventListener('click', () => {
-  currentPosition -= slideWidth;
-  if (currentPosition < -(slider.scrollWidth - slider.offsetWidth)) {
-    currentPosition = 0;
-  }
-  slider.style.transform = `translateX(${currentPosition}px)`;
-});
+function Prev() {   
+    let sliderSection = document.querySelectorAll('.slider_section');
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = '0';
+    slider.style.transition = 'all 0.5s';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){ Next(); });
+btnLeft.addEventListener('click', function(){ Prev(); });
